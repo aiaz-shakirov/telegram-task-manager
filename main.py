f01e36f3036ed848e15a766db136d7c67a31277f
Aiaz Shakirov <ayazshakirov007@icloud.com>
2026-05-12 16:59:29 +0300

    Основа бота разработана, отвечает лишь на /start

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-for i in range(101):
-    if i % 2 == 0:
-        print("i is even")
-        break
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import os
+from dotenv import load_dotenv
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.first_name}!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
